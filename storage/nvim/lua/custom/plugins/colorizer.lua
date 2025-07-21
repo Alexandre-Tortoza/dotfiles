a1b8f8551eb01e7e170398b9976245ad446bb1ec
Alexandre-Tortoza <a.marquestortoza@gmail.com>
2025-07-21 07:35:59 -0300

    .

--- storage/nvim/lua/custom/plugins/colorizer.lua
+++ storage/nvim/lua/custom/plugins/colorizer.lua
@@ -2,15 +2,15 @@ return {
   'NvChad/nvim-colorizer.lua',
   opts = {
     filetypes = { '*' },
-    user_default_options = {
-      names = true,
-      rgb_fn = true,
-      hsl_fn = true,
-      css = true,
-      css_fn = true,
-      mode = 'virtualtext',
-      virtualtext = '■',
-      -- virtualtext = "🐒",
-    },
+    --   user_default_options = {
+    --     names = true,
+    --     rgb_fn = true,
+    --     hsl_fn = true,
+    --     css = true,
+    --     css_fn = true,
+    --     mode = 'virtualtext',
+    --     virtualtext = '■',
+    --     -- virtualtext = "🐒",
+    --   },
   },
 }
